18253fbd6f1cbac791897e27ca414d6e5db5e615
abhyvoid <abhayrangappanvat@gmail.com>
2025-08-19 21:18:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function login() {
+  let u = document.getElementById("username").value;
+  let p = document.getElementById("password").value;
+  if(u === "admin" && p === "1234") {
+    document.getElementById("login-box").style.display = "none";
+    document.getElementById("content").style.display = "block";
+    showPage('home');
+  } else {
+    alert("❌ Wrong username or password!");
+  }
+}
+
+function showPage(pageId) {
+  let pages = document.getElementsByClassName("page");
+  for (let i = 0; i < pages.length; i++) {
+    pages[i].style.display = "none";
+  }
+  document.getElementById(pageId).style.display = "block";
+}
+
+function logout() {
+  document.getElementById("content").style.display = "none";
+  document.getElementById("login-box").style.display = "block";
+}
